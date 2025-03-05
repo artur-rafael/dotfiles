c16ce868bb675a161dd5befb6a3b7bcfb246751e
Artur Rafael <76266382+artur-rafael@users.noreply.github.com>
2025-03-05 23:51:00 +0000

    fix fzf submodule and paths

--- .bash/fzf.bash
+++ .bash/fzf.bash
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/arrr/.fzf/bin* ]]; then
-   PATH="${PATH:+${PATH}:}/home/arrr/.fzf/bin"
+if [[ ! "$PATH" == *"$HOME/.fzf/bin"* ]]; then
+   PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 eval "$(fzf --bash)"
